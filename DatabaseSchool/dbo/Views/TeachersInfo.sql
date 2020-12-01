af3f50fd28bc238575928887ec5196a72f12ee7f
nebosky142 <45599477+nebosky142@users.noreply.github.com>
2020-12-01 08:16:11 +0200

    Update: Add TeachersInfo view

---
+++ DatabaseSchool/dbo/Views/TeachersInfo.sql
@@ -0,0 +1,3 @@
+﻿CREATE VIEW TeachersInfo AS
+SELECT TeacherId, CONCAT(FirstName, ' ', Patronymic) AS TeacherPartName, CONCAT(SurName, ' ', FirstName, ' ', Patronymic) AS TeacherFullName
+FROM Teachers;
