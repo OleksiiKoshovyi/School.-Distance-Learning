89852375ee2ee3e863a4b3f0244551fc1c1ff10c
nebosky142 <45599477+nebosky142@users.noreply.github.com>
2021-01-14 03:04:42 +0200

    Fix minor bug: wrong attribute in query (groups)

--- School. Distance Learning/Areas/Pupil/Controllers/HomeworkController.cs
+++ School. Distance Learning/Areas/Pupil/Controllers/HomeworkController.cs
@@ -22,63 +22,6 @@ namespace School._Distance_Learning.Areas.Pupil.Controllers
             _context = context;
         }
 
-        /*private IndexViewModel GetIndexViewModel(int pupilid)
-        {
-            Pupils pupil = _context.Pupils
-                .Where(p => p.PupilId == pupilid)
-                .FirstOrDefault();
-
-            Grades grade = _context.Pupils
-                .Include(p => p.Grade)
-                .Where(p => p.PupilId == pupilid)
-                .Select(g => g.Grade)
-                .FirstOrDefault();
-
-            List<Groups> groups = _context.GroupPupil
-                .Include(gp => gp.Group)
-                .Include(gp => gp.Pupil)
-                .Where(gp => gp.Pupil.PupilId == pupilid)
-                .Select(gp => gp.Group)
-                .ToList();
-
-            var tt = _context.Timetables
-                 .Include(t => t.TeacherSubjectGroup)
-                 .Include(t => t.TeacherSubjectGroup.TeacherSubject)
-                 .Include(t => t.TeacherSubjectGroup.TeacherSubject.Subject)
-                 .Include(t => t.TeacherSubjectGroup.TeacherSubject.Teacher)
-                 .Include(t => t.TeacherSubjectGroup.Group)
-                 .Include(t => t.TeacherSubjectGroup.Group.GroupType)
-                 .Include(t => t.TeacherSubjectGroup.Group.Grade)
-                 .Where(t => t.TeacherSubjectGroup.Group.GradeId == grade.GradeId)
-                 .Where(t => t.TeacherSubjectGroup.Group.GroupTypeId == null || groups.Contains(t.TeacherSubjectGroup.Group))
-                 .OrderBy(t => t.TeacherSubjectGroup.Group.GradeId)
-                 .ThenBy(t => t.WeekDayNumber)
-                 .ThenBy(t => t.LessonNumber)
-                 .ThenBy(t => t.TeacherSubjectGroup.GroupId)
-                 .ToList();
-
-            int maxLessonNumber = tt.Select(t => t.LessonNumber).DefaultIfEmpty().Max();
-
-            List<List<List<Timetables>>> lessons =
-                new List<List<List<Timetables>>>(maxLessonNumber);
-
-            for (int i = 0; i < maxLessonNumber; i++)
-            {
-                lessons.Add(new List<List<Timetables>>(5));
-                for (int j = 0; j < 5; j++)
-                {
-                    lessons[i].Add(new List<Timetables>());
-                }
-            }
-
-            foreach (var lesson in tt)
-            {
-                lessons[lesson.LessonNumber - 1][lesson.WeekDayNumber - 1].Add(lesson);
-            }
-
-            return new IndexViewModel(lessons, pupil, DateTime.Now);
-        }*/
-
         // GET: TimetableForPupilsController
         public async Task<ActionResult> IndexAsync(int? pupilid, DateTime? date)
         {
@@ -111,7 +54,7 @@ namespace School._Distance_Learning.Areas.Pupil.Controllers
                     string groups =
                         " SELECT Groups.GroupId FROM Groups " +
                         "RIGHT JOIN GroupPupil gp ON gp.GroupId = Groups.GroupId " +
-                        $"WHERE Groups.GroupId = {pupilid} ";
+                        $"WHERE gp.PupilId = {pupilid} ";
 
                     string query =
                         "SELECT tt.TimetableId, tt.TeacherSubjectGroupId, hw.HomeworkId, tt.LessonNumber, s.SubjectName, hw.Homework " +
